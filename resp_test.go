b93c555085360fa93eec1cefb1d16b52c22b74ca
Prthmesh6 <prathmeshpatilp@gmail.com>
2024-01-18 00:02:05 +0530

    added support for Redis commands

--- resp_test.go
+++ resp_test.go
@@ -58,3 +58,46 @@ func TestResp_readLine(t *testing.T) {
 		})
 	}
 }
+
+func TestResp_readInteger(t *testing.T) {
+	type fields struct {
+		reader *bufio.Reader
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantX   int
+		wantN   int
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "testReadInteger",
+			fields: fields{
+				reader: getReader("369\r\n"),
+			},
+			wantX:   369,
+			wantN:   5,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &Resp{
+				reader: tt.fields.reader,
+			}
+			gotX, gotN, err := r.readInteger()
+			fmt.Printf("gotX = %v, gotN = %v, err = %v", gotX, gotN, err)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Resp.readInteger() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotX != tt.wantX {
+				t.Errorf("Resp.readInteger() gotX = %v, want %v", gotX, tt.wantX)
+			}
+			if gotN != tt.wantN {
+				t.Errorf("Resp.readInteger() gotN = %v, want %v", gotN, tt.wantN)
+			}
+		})
+	}
+}
